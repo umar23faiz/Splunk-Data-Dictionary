531d53380fcd320cf231f7fe4abbd0977af63dd1
umar23faiz <umar23faiz@gmail.com>
2024-01-13 17:06:00 +0530

    Added styles

--- packages/frontend/src/pages/Overview.tsx
+++ packages/frontend/src/pages/Overview.tsx
@@ -24,57 +24,78 @@ function Overview({ apiData }){
         border: `1px solid ${syntaxBlue}`,
         padding: 10,
         minHeight: 30,
-        width: 90,
+        width: 190,
         marginBottom:19,
         marginLeft:5,
         borderRadius: 6
 
     };
+    const divStyles = {
+        marginLeft: '15%',
+        fontWeight: 'bolder',
+        fontSize: 'x-large',
+      };
+      const nbrStyles = {
+        marginLeft: '28%',
+        fontWeight: 'bolder',
+        fontSize: '3rem',
+        padding:8
+      };
+
+
 
 return(
    <div className="rounded-md" style={padding}>
             <div style={buttonWidth}>
-                {apiData.customApps} Custom Apps
+            <div style={nbrStyles}> {apiData.customApps}  </div> <br /> <div  style={divStyles}>  Custom Apps </div>
             </div>
             <ColumnLayout  style={rowLayout} >
                 <ColumnLayout.Row>
                     <ColumnLayout.Column gutter={5} span={3} style={colStyle}>
-                    {apiData.dashboard} Dashboards
+                    <div style={nbrStyles}>{apiData.dashboard}</div> <br/><div style={divStyles}>
+                        Dashboards
+                        </div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={3} style={colStyle}>
-                    {apiData.report} Reports
+                    <div style={nbrStyles}>
+                        {apiData.report} 
+                        </div>
+                        <br />
+                    <div  style={divStyles}>
+                         Reports
+                        </div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={3} style={colStyle}>
-                    {apiData.report}  Searches
+                    <div style={nbrStyles}>{apiData.report} </div> <br /> <div  style={divStyles}>Searches</div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={3} style={colStyle}>
-                    {apiData.lookup}  Lookups
+                    <div style={nbrStyles}>{apiData.lookup}  </div> <br /> <div  style={divStyles}> Lookups</div>
                     </ColumnLayout.Column>
                 </ColumnLayout.Row>
             </ColumnLayout>
             <ColumnLayout  style={rowLayout} >
                 <ColumnLayout.Row>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                    {apiData.fields}   Unique fields
+                    <div style={nbrStyles}>{apiData.fields}   </div> <br /> <div  style={divStyles}>  Unique fields </div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                    {apiData.index}  Index
+                    <div style={nbrStyles}>{apiData.index}  </div> <br /> <div  style={divStyles}>  Index</div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                    {apiData.sourceType}  SourceTypes 
+                    <div style={nbrStyles}>{apiData.sourceType}   </div> <br /> <div  style={divStyles}> SourceTypes </div>
                     </ColumnLayout.Column>
                 </ColumnLayout.Row>
             </ColumnLayout>
              <ColumnLayout style={rowLayout} >
                 <ColumnLayout.Row>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                        meta
+                    <div></div> <br /> <div  style={divStyles}>  meta </div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                       eventtype / tags
+                    <div></div> <br /> <div  style={divStyles}>  eventtype / tags </div>
                     </ColumnLayout.Column>
                     <ColumnLayout.Column span={4} style={colStyle}>
-                        labeling / Classification
+                    <div></div> <br /> <div  style={divStyles}>  labeling / Classification </div>
                     </ColumnLayout.Column>
                 </ColumnLayout.Row>
             </ColumnLayout>
